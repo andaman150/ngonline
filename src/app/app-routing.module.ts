3d8fb4783493804ab845c1ab53fe78a68ae9a1dd
andaman150 <andaman.nr@gmail.com>
2022-01-10 16:07:54 +0700

    add product model, service, component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path : '', component:HomeComponent},
-  {path : 'about', component:AboutComponent}
+  {path : 'about', component:AboutComponent},
+  {path : 'product', component:ProductsComponent},
 ];
 
 @NgModule({
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,26 @@
+import { Product } from './shared/product.model';
+import { ProductService } from './shared/product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  porducts : Product[];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.productService.getProduct().subscribe(
+      (products) => {
+        // console.log(products);
+        this.porducts = products;
+
+      }
+    );
+  }
+
+}
