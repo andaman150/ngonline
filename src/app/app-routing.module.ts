b787631d7d55e2b250cf701df7688aa7401f9885
andaman150 <andaman.nr@gmail.com>
2022-01-11 10:04:09 +0700

    add Feature Module dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { NewsComponent } from './news/news.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
@@ -12,11 +13,13 @@ const routes: Routes = [
   {path : 'product', component:ProductsComponent},
   {path : 'product/:id', component:ProductDetailComponent},
   {path : 'news', component:NewsComponent},
+  {path : 'dashboard', loadChildren:'./dashboard/dashboard.module#DashboardModule'},
 
+  {path : '**', component:PagenotfoundComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash:true, preloadingStrategy:PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
